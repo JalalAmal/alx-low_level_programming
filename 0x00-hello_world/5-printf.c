81970669bbab7826827e2d1ff9d22c59310d3d36
Jalal Amal <jalal.amal01@yahoo.com>
2023-03-16 06:46:14 -0700

    update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+ * main - function that prints a given input within printf
+ * Return: 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
