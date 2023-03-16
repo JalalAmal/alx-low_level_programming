a9bd99aa464c7432a58eb0a71f299748aab2a901
Jalal Amal <jalal.amal01@yahoo.com>
2023-03-16 05:21:11 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
  * Return: Always 0 (Success)
  */
 #include <stdio.h>
-/*
- * This function prints Programming is like building a multilingual puzzle
- */
+
+// This function prints Programming is like building a multilingual puzzle
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
